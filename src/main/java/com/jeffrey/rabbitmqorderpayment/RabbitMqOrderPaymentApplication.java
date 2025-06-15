ebe9c1d1bc53ccb65e8680379f3c08d11633c9fe
오태현 <jeffreyoh121@gmail.com>
2025-06-15 18:31:54 +0900

    Init

---
+++ src/main/java/com/jeffrey/rabbitmqorderpayment/RabbitMqOrderPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.jeffrey.rabbitmqorderpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqOrderPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqOrderPaymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeffrey/rabbitmqorderpayment/RabbitMqOrderPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffrey.rabbitmqorderpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqOrderPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
